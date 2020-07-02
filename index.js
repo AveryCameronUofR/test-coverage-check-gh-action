0d0e42f2854aecb189d85d58c5c3ac7384f5471a
Avery Cameron <35551198+AveryCameronUofR@users.noreply.github.com>
2020-07-02 17:36:45 -0400

    Fixed use of since and removed redundant access line

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ const core = require('@actions/core');
  * coverage XML reports. Uses regular expression to find test coverage results
  * from each report for the file and the coverage value is in the first group of the match.
  * 
- * @since 2020.07.02
- * @access public
+ * @since 1.0.0
  * 
- * @param {String} filename name of file to compare coverage
- * @param {Number} minCoverage minimum coverage to pass check
- * @param {Number} maxCoverageChange max coverage change to pass check
+ * @param {String} filename name of file to compare coverage.
+ * @param {Number} minCoverage minimum coverage to pass check.
+ * @param {Number} maxCoverageChange max coverage change to pass check.
  * 
- * @return {Number} Change in coverage
+ * @return {Number} Change in coverage.
  */
 function compareCoverage(filename, minCoverage, maxCoverageChange){
   
@@ -27,12 +26,11 @@ function compareCoverage(filename, minCoverage, maxCoverageChange){
  * for use with finding and comparing test coverage. The input filename 
  * determines which file to put in the regular expression.
  * 
- * @since 2020.07.02
- * @access public
+ * @since 1.0.0
  * 
- * @param {String} filename Name of file to match
+ * @param {String} filename Name of file to match.
  * 
- * @return {RegExp} Regular Expression that matches filename and provides coverage value as a group
+ * @return {RegExp} Regular Expression that matches filename and provides coverage value as a group.
  */
 function makeRegEx(filename){
   return new RegExp(`<class name="\w*.py" filename="${filename}" complexity="\d*" line-rate="(\d*\.?\d*)" branch-rate=...>`)
